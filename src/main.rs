ed81818a3a074981754224d94d445f61195192c7
Flawn <fl4wn@protonmail.com>
2023-07-27 11:32:07 +0200

    ✨ First bad attempt to get stage 2 rigt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::net::TcpListener;
+use std::{net::{TcpListener, TcpStream}, thread, io::{Read, Write}};
+
 
 fn main() {
-    println!("Logs from your program will appear here!");
+    println!("Server started up! 💯");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
@@ -9,9 +10,36 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+
+                thread::spawn(move || {
+                    handle_client(_stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+                let message = String::from_utf8_lossy(&buffer[..n]);
+                println!("Received message: {}", message);
+                
+                if message.contains("ping"){
+                    let _ = stream.write_all(b"+PONG\r\n").unwrap();
+                }
+            }
+            Err(e) => {
+                println!("Error reading from stream: {}", e);
+                break;
             }
         }
     }

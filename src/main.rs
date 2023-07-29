5206bfc6bdd2849d239ad238d83dc0576b8cb9fa
Flawn <fl4wn@protonmail.com>
2023-07-29 15:56:38 +0200

    ✨ Add SET & GET

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,20 @@ use std::{
 };
 
 use crate::message::message::Message;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 mod message;
+
+struct AppState {
+    dict: Mutex<HashMap<String, String>>,
+}
 
 fn main() {
     println!("Server started up! 💯");
+
+    let app_state = Arc::new(AppState {
+        dict: Mutex::new(HashMap::new()),
+    });
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
@@ -17,8 +27,10 @@ fn main() {
             Ok(_stream) => {
                 println!("accepted new connection");
 
+                let app_state = app_state.clone();
+
                 thread::spawn(move || {
-                    handle_client(_stream);
+                    handle_client(_stream, &app_state);
                 });
             }
             Err(e) => {
@@ -28,7 +40,7 @@ fn main() {
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, app_state: &Arc<AppState>) {
     let mut buffer = [0; 1024];
     loop {
         match stream.read(&mut buffer) {
@@ -58,6 +70,54 @@ fn handle_client(mut stream: TcpStream) {
                             ]
                             .concat();
                             let _ = stream.write_all(&response[..].as_bytes()).unwrap();
+                        }
+
+                        if message.command == "get" {
+                            let mut dict = app_state.dict.lock().unwrap();
+
+                            let nil_value: String = "nil".to_string();
+
+                            let val = dict.get(&message.args[1]).unwrap_or(&nil_value);
+
+                            let response = [
+                                "$",
+                                val.len().to_string().as_str(),
+                                "\r\n".to_string().as_str(),
+                                (val).as_str(),
+                                "\r\n".to_string().as_str(),
+                            ]
+                            .concat();
+                            let _ = stream.write_all(&response[..].as_bytes()).unwrap();
+
+                            drop(dict);
+                        }
+
+                        if message.command == "set" {
+                            let mut dict = app_state.dict.lock().unwrap();
+
+                            let ret: String = "OK".to_string();
+
+                            let insert_op =
+                                dict.insert(message.args[1].clone(), message.args[2].clone());
+
+                            // TODO: Add returning old value
+                            match insert_op {
+                                Some(old) => (),
+                                None => (),
+                            }
+
+                            let response = [
+                                "$",
+                                ret.len().to_string().as_str(),
+                                "\r\n".to_string().as_str(),
+                                (ret).as_str(),
+                                "\r\n".to_string().as_str(),
+                            ]
+                            .concat();
+
+                            let _ = stream.write_all(&response[..].as_bytes()).unwrap();
+
+                            drop(dict);
                         }
                     }
                     Err(error) => {
